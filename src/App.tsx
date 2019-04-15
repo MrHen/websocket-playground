bdaa10372a986bda51f47477a4d91276d2820868
Adam Babcock <github@mrhen.com>
2019-04-15 10:39:11 -0500

    Render basic canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,107 @@
+import { Delaunay } from "d3-delaunay";
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  canvasRef = React.createRef<HTMLCanvasElement>();
+
+  width = 600;
+
+  height = 600;
+
+  points = [
+    [100, 100],
+    [100, 500],
+    [300, 300],
+    [500, 100],
+    [500, 500],
+  ];
+
+  componentDidMount() {
+    const {
+      canvasRef,
+      renderCanvas,
+    } = this;
+
+    const canvas = canvasRef.current;
+    let context = null;
+
+    if (canvas) {
+      context = canvas.getContext('2d');
+    }
+
+    if (context) {
+      renderCanvas(context);
+    }
+  }
+
+  get delaunay() {
+    const {
+      points,
+    } = this;
+
+    return Delaunay.from(points);
+  }
+
+  get voronoi() {
+    const {
+      delaunay,
+      height,
+      points,
+      width,
+    } = this;
+
+    return delaunay.voronoi([0, 0, width, height]);
+  }
+
+  renderCanvas = (context: CanvasRenderingContext2D) => {
+    const {
+      canvasRef,
+      delaunay,
+      voronoi,
+    } = this;
+
+    console.log('renderCanvas started', {
+      context,
+      delaunay,
+      voronoi,
+    });
+
+    context.setLineDash([6, 2]);
+    context.beginPath();
+    context.strokeStyle = 'lightblue';
+    delaunay.render(context);
+    context.stroke();
+
+    context.setLineDash([1, 0]);
+    context.beginPath();
+    context.strokeStyle = 'black';
+    voronoi.render(context);
+    context.stroke();
+
+    context.beginPath();
+    context.fillStyle = 'blue';
+    delaunay.renderPoints(context);
+    context.fill();
+  }
+
   render() {
+    const {
+      canvasRef,
+      height,
+      width,
+    } = this;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <canvas
+          ref={canvasRef}
+          height={height}
+          width={width}
+        />
       </div>
     );
   }
